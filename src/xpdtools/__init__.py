c56205219194667d4e6f9943763912e8df7ea41c
Jakub Wlodek <jwlodek@bnl.gov>
2026-07-07 12:23:31 -0400

    Initial commit

---
+++ src/xpdtools/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2026 Brookhaven National Laboratory. All rights reserved.
+
+xpdtools: Tools for NSLS-II XPD beamline
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
